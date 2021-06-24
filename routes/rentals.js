e17690effb00cc9d98c5740d1aed6d3cb0347f0d
shreyanshvarshney <varshney.shreyansh@gmail.com>
2021-06-24 20:16:47 +0530

    added Transaction in rental post route using two phase commit from Fawn Library

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const Fawn = require("fawn");
+const mongoose = require("mongoose");
 const { Rental, validateRental } = require("../models/rental");
 const { Movie } = require("../models/movie");
 const { Customer } = require("../models/customer");
+
+Fawn.init(mongoose);
 
 router.get("", async (req, res, next) => {
     const result = await Rental.find().sort({dateOut: -1}).select({__v: false});
@@ -48,19 +52,42 @@ router.post("", async (req, res, next) => {
     // This will create data inconsistency.
     // So now using transaction concept I want to run them as single unit operation. If one fails none of them will be executed.
     // And my db will be in the initial state as it was before both operations.
+
+    const task = new Fawn.Task();
+    // Now here I can add one or more operations and all these operations together will be treated as a unit.
+    // Here I am directly working with the collection, so i have to provide name of the collection which is prural and lowercase.
+    // Second argument will be my Rental Model object to be posted.
+
+    // There maybe chances that this transaction fails so i will wrap this in try catch block:
     try {
-        const result = await rental.save();
-        res.status(201).json({message: "Successfully Rented", data: result});
+        task.save("rentals", rental)
+        .update("movies", {_id: movie._id}, { $inc: { numberInStock: -1 } })
+        .run();
+        // No changes will be made to my database until I call task.run();
+
+        // [!!IMPORTANT] concept of this below retal object of Rental model i have created
+        // I have not mentioned _id and dateOut fields when creating rental obejct. But in the body of the reponse to the client I can see boht there.
+        // So, MongoDB Database DID NOT set these values, I define them in Mongoose Schema.
+        // So when I create a new rental object, mongoose knew my rental schema and look for various properties and default values.
+        // These both properties values were SET before I save this document to the DB by the MongoDB Driver.
+        res.status(201).json({message: "Successfully Rented", data: rental});
     } catch (err) {
-        return res.status(500).json({message: err.message});
+        res.status(500).json({message: err.message});
     }
+
+    // try {
+    //     const result = await rental.save();
+    //     res.status(201).json({message: "Successfully Rented", data: result});
+    // } catch (err) {
+    //     return res.status(500).json({message: err.message});
+    // }
     
-    const body = {
-        $inc: {
-            numberInStock: -1
-        }
-    };
-    await Movie.updateOne({_id: movie._id}, body);
+    // const body = {
+    //     $inc: {
+    //         numberInStock: -1
+    //     }
+    // };
+    // await Movie.updateOne({_id: movie._id}, body);
     // movie.numberInStock--;
     // movie.save();
 });
